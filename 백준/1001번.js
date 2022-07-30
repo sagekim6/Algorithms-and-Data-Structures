24544cbadf1ecd84864a6a02be133c7242ce36fc
KimSeYoung <sagekim6@gmail.com>
2022-07-30 13:12:08 +0900

    node.js 입력받기 성공, 백준 1001번

--- 백준/1001번.js
+++ 백준/1001번.js
@@ -1,1 +1,4 @@
-console.log("hello wrold!");
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+console.log(A - B);
